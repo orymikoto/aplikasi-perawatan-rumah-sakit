90ec5352765627e2b2996a89aaf9d3cf392b3210
orymikoto <rosbergroll99@gmail.com>
2024-06-18 17:03:07 +0700

    seeder petugas bagas

--- database/seeders/DataPetugasSeeder.php
+++ database/seeders/DataPetugasSeeder.php
@@ -17,7 +17,36 @@ class DataPetugasSeeder extends Seeder
   {
     $faker = FakerFactory::create();
     $role = ["ADMIN", "KEPALA", "PERAWAT", "PETUGAS"];
-    $data = [];
+    $data = [
+      [
+        'nama' => "Bagas Febriansyah",
+        'email' => "adminbagas@dkt.com",
+        'role' => "ADMIN",
+        'password' => Hash::make('12345678'),
+        'foto_profil' => $faker->imageUrl()
+      ],
+      [
+        'nama' => "Bagas Febriansyah",
+        'email' => "petugasbagas@dkt.com",
+        'role' => "PETUGAS",
+        'password' => Hash::make('12345678'),
+        'foto_profil' => $faker->imageUrl()
+      ],
+      [
+        'nama' => "Bagas Febriansyah",
+        'email' => "kepalabagas@dkt.com",
+        'role' => "KEPALA",
+        'password' => Hash::make('12345678'),
+        'foto_profil' => $faker->imageUrl()
+      ],
+      [
+        'nama' => "Bagas Febriansyah",
+        'email' => "perawatbagas@dkt.com",
+        'role' => "PERAWAT",
+        'password' => Hash::make('12345678'),
+        'foto_profil' => $faker->imageUrl()
+      ],
+    ];
     for ($i = 0; $i < 8; $i++) {
       array_push($data, [
         'nama' => $faker->name(),
